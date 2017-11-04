488029c53783c685bd3952961120994b077afdeb
= <=>
2017-11-05 01:10:34 +0530

    new build

--- build/scripts/start.js
+++ build/scripts/start.js
@@ -1,6 +1,7 @@
 const logger = require('../lib/logger')
 
 logger.info('Starting server...')
-require('../../server/main').listen(process.env.PORT || 8000, () => {
-  logger.success('Server is running at http://localhost:3000')
+var port = process.env.PORT || 8000
+require('../../server/main').listen(port, () => {
+  logger.success('Server is running at http://localhost:'+port)
 })
